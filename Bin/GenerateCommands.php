d3b88839b60986a40f20ffa2dedc5416d3a017b3
Steel Brain <aneesiqbalbhatti@gmail.com>
2015-05-12 23:34:11 -0700

    :new: Add GenerateCommands.php

---
+++ Bin/GenerateCommands.php
@@ -0,0 +1,4 @@
+<?php
+$Names = array_keys(json_decode(file_get_contents("http://redis.io/commands.json"), 1));
+$Target = __DIR__.'/../Dist/Commands.js';
+file_put_contents($Target, "module.exports = ".json_encode($Names).';');
