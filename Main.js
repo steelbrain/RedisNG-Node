66df4558f69c9b1bd64dc5c44f2351ada54a3fc3
Steel Brain <aneesiqbalbhatti@gmail.com>
2015-05-13 01:35:57 -0700

    :no_entry: Disallow running on normal nodejs

--- Main.js
+++ Main.js
@@ -4,5 +4,5 @@ var IsIoJS = parseInt(process.version.substr(1)) > 0;
 if(IsIoJS){
   module.exports = require('./Source/Main');
 } else {
-  module.exports = require('./Dist/Main');
+  throw new Error("Redis-Promise consumes generators so is only supported on io.js");
 }
