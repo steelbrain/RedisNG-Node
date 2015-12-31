ec429f43bb6a35cd4db0b5947024680e2ac9a520
steelbrain <aneesiqbalbhatti@gmail.com>
2015-12-31 05:36:19 -0700

    :new: Add specs

---
+++ spec/spec-pubsub.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const RedisNG = require('../').RedisNG
+
+const redis = new RedisNG()
+const publisher = new RedisNG()
+redis.on('message', function(channel, message) {
+  console.log('global handler triggered', channel, message)
+})
+redis.on('message:CHAN', function(message) {
+  console.log('channel handler triggered', message)
+  publisher.unref()
+  redis.unref()
+})
+redis.connect().then(function() {
+  return redis.subscribe('CHAN').then(function() {
+    return publisher.connect()
+  }).then(function() {
+    publisher.publish('CHAN', 'Message')
+  })
+}, function(e) {
+  console.log(e.message, e.stack)
+})
