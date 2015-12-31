5af124342407bc1150aeca1f44b05d5abefc69b3
steelbrain <aneesiqbalbhatti@gmail.com>
2015-12-31 05:58:39 -0700

    :art: Simplify spec

--- spec/spec-main.js
+++ spec/spec-main.js
@@ -5,12 +5,12 @@ const RedisNG = require('../').RedisNG
 const redis = new RedisNG()
 redis.connect().then(function() {
   console.log('connected')
-  return redis.set('KEY', 'VALUE').then(function(result) {
+  return redis.set('KEY', 'VALUE').then(function() {
     console.log('value set')
-    return redis.get('KEY').then(function(result) {
-      console.log(result === 'VALUE' ? 'result matches' : 'result mismatches')
-      redis.unref()
-    })
+    return redis.get('KEY')
+  }).then(function(result) {
+    console.log(result === 'VALUE' ? 'result matches' : 'result mismatches')
+    redis.unref()
   })
 }, function(e) {
   console.log(e.message, e.stack)
